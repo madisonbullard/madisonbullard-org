05cf1be0df64ac161d7a2f786d5ef38e4d296a59
Madison Bullard <madison.bullard@gmail.com>
2022-05-28 00:40:16 -0400

    cleanup

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  // Run type-check on changes to TypeScript files
   "**/*.ts": () => "pnpm type-check",
-  // Run ESLint on changes to JavaScript/TypeScript files
   "**/*.(ts|js)": (filenames) =>
     `pnpm lint --cache --fix . ${filenames.join(" ")}`,
   "**/*.{js,ts,css,md}": "prettier --write",
